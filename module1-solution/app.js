e5a93de6476c16a9f228ac53a3eacd452b0c632d
lwallen <lwallen@gettyimages.com>
2016-11-13 03:54:57 -0800

    Refactoring after getting feedback.

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,30 +4,61 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
+LunchCheckController.$inject = ['$scope', '$filter'];
+function LunchCheckController($scope, $filter) {
 
+  $scope.colors ={};
+  $scope.colors.current = {color: "green"};
   $scope.lunchItems = "";
   $scope.numItemsMessage = "";
 
   $scope.checkNumItems = function() {
-
-    if ($scope.lunchItems == "") {
-      $scope.numItemsMessage = "Please enter data first";
-      return;
-    }
-
-    var lunchItemsArray = $scope.lunchItems.split(',');
-
-    if (lunchItemsArray.length <= 3) {
-      $scope.numItemsMessage = "Enjoy!";
-    } else {
-      $scope.numItemsMessage = "Too much!";
+    var numItems = $scope.getNumItems();
+    switch(numItems) {
+      case 0:
+        $scope.numItemsMessage = "Please enter data first";
+        $scope.turnOrange();
+        break;
+      case 1:
+      case 2:
+      case 3:
+        $scope.numItemsMessage = "Enjoy!";
+        $scope.turnGreen();
+        break;
+      default:
+          $scope.numItemsMessage = "Too much!";
+          $scope.turnRed();
+          break;
     }
   };
 
   $scope.showNumItemsMessage = function() {
-      return $scope.numItemsMessage;
+    return $scope.numItemsMessage;
   }
+
+  $scope.getNumItems = function() {
+    var lunchItemsArray = $scope.lunchItems.split(',');
+    var count = 0;
+
+    for (var i = 0; i < lunchItemsArray.length; i++) {
+      if (lunchItemsArray[count]) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  $scope.turnGreen = function (){
+    $scope.colors.current = {color: "green"};
+  };
+
+  $scope.turnRed = function (){
+    $scope.colors.current = {color: "red"};
+  };
+
+  $scope.turnOrange = function (){
+    $scope.colors.current = {color: "orange"};
+  };
+
 }
 })();
